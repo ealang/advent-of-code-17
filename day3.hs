87d62ed5aae9525350d98a18d84add5a53a186f8
Eric Lang <ealang@gmail.com>
2018-09-10 21:24:27 -0500

    cleanup and optimize day3

--- day3.hs
+++ day3.hs
@@ -2,50 +2,36 @@ import Text.Printf
 import qualified Data.Map as Map
 import Data.List (find)
 
-numToPos :: Int -> (Int, Int)
-numToPos 1 = (0, 0)
-numToPos num = fromShell 1 (num - 2)
-  where fromShell curShell i
-         | i < tr =
-            (curShell, i - middle)
-         | i < tl =
-            (middle - (i - tr), curShell)
-         | i < bl =
-            (-curShell, middle - (i - tl))
-         | i < br =
-            ((i - bl) - middle, -curShell)
-         | otherwise =
-            fromShell (curShell + 1) (i - br)
-         where middle = curShell - 1
-               sideLen = curShell * 2
-               tr = sideLen
-               tl = tr + sideLen
-               bl = tl + sideLen
-               br = bl + sideLen
+cellPositions :: [(Int, Int)]
+cellPositions = fromShell 0
+  where fromShell 0 = (0, 0): fromShell 1
+        fromShell shell = [(shell, y) | y <- [(1 - shell) .. shell]] ++
+                          [(x, shell) | x <- reverse [-shell .. (shell - 1)]] ++
+                          [(-shell, y) | y <- reverse [-shell .. (shell - 1)]] ++
+                          [(x, -shell) | x <- [(1 - shell) .. shell]] ++
+                          fromShell (shell + 1)
 
 neighbors :: (Int, Int) -> [(Int, Int)]
-neighbors (x, y) = [ (x - 1, y), (x + 1, y),
-                     (x, y - 1), (x, y + 1),
-                     (x - 1, y - 1), (x - 1, y + 1),
-                     (x + 1, y + 1), (x + 1, y - 1) ]
+neighbors (x, y) = [ (x + dx, y + dy) |
+                     dx <- [-1..1], dy <- [-1..1],
+                     dx /= 0 || dy /= 0 ]
+
+cellSums :: [Int]
+cellSums = map snd $ scanl nextSum (initCache, 1) (drop 1 cellPositions)
+  where nextSum (cache, _) pos = let
+          cellVal = sum $ map (\p -> Map.findWithDefault 0 p cache)
+                              (neighbors pos)
+          in (Map.insert pos cellVal cache, cellVal)
+        initCache = Map.fromList [((0, 0), 1)]
 
 day3Part1 :: Int -> Int
 day3Part1 num = abs x  + abs y
-  where (x, y) = numToPos num
+  where (x, y) = cellPositions !! (num - 1)
 
 day3Part2 :: Int -> Maybe Int
-day3Part2 targetVal = find (>targetVal) cellValues
-  where fReduce (cache, _) curNum = let
-          cellPos = numToPos curNum
-          positions = neighbors cellPos
-          valAtPos pos = Map.findWithDefault 0 pos cache
-          cellVal = sum $ map valAtPos positions
-          in (Map.insert cellPos cellVal cache, cellVal)
-        initCache = Map.fromList [((0, 0), 1)]
-        cellValues = map snd $ scanl fReduce (initCache, 1) [2..]
+day3Part2 targetVal = find (>targetVal) cellSums
 
 main = do
   let input = 277678
   putStrLn $ "part1: " ++ show (day3Part1 input)
   putStrLn $ "part2: " ++ show (day3Part2 input)
-
